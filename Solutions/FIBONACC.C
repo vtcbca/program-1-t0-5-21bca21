f810eb264d1e33bae6337c5b0d82989e50f6efa5
21bca21 <96718294+21bca21@users.noreply.github.com>
2022-01-05 10:30:52 +0530

    Add files via upload

---
+++ Solutions/FIBONACC.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int n1=0,n2=1,n3,i,number;
+	clrscr();
+	printf("Enter the number of element:");
+	scanf("%d",&number);
+	printf("\n %d%d",n1,n2);
+	for(i=2;i<number;i++)  //loop start from 2 beacuse 0 & 1 already printed//
+	{
+		n3=n1+n2;
+		printf("%d",n3);
+		n1=n2;
+		n2=n3;
+	}
+	getch();
+}
