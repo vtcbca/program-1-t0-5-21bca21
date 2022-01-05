4016bef0b7d2727f29d59db4c0c47ad34040a07c
21bca21 <96718294+21bca21@users.noreply.github.com>
2022-01-05 10:30:26 +0530

    Add files via upload

---
+++ Solutions/ARMSTRON.C
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,c,d=0;
+	clrscr();
+	printf("Enter digit no:");
+	scanf("%d",&a);
+	b=a;
+	while(a>0)
+	{
+		c=a%10;
+		d=d+c*c*c;
+		a=a/10;
+	}
+	if(d==b)
+	{
+		printf("Number is armstrong");
+	}
+	else
+	{
+		printf("Number is not armstrong");
+	}
+	getch();
+}
